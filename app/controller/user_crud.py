0682d77bbc7b1a07e7131455e655dedf78a77da7
amelia <tndus2623@gmail.com>
2024-04-22 19:57:23 +0900

    [etc] 유저 정보 업데이트 API 로직 수정

--- app/controller/user_crud.py
+++ app/controller/user_crud.py
@@ -28,20 +28,22 @@ def token_issued(token,response:Response = None):
 
 
 def update_info(data:schemas.UpdateUser,user,session:Session, response:Response = None):
-    update_data = {}
-    if data.new_pw:
-        update_data['pw'] = hash_password(data.new_pw)
-    if data.new_name:
-        update_data['name'] = data.new_name
-    if update_data:
-        models.Users.update(session, user.id, **update_data)
-        token = user.get_token(session)
-        response.headers['Access-Token'] = token['access_token']
-        response.headers['Refresh-Token'] = token['refresh_token']
-        return {"value" : "Change Success"}
+    if user.pw == data.curr_pw:
+        update_data = {}
+        if data.new_pw:
+            update_data['pw'] = hash_password(data.new_pw)
+        if data.new_name:
+            update_data['name'] = data.new_name
+        if update_data:
+            models.Users.update(session, user.id, **update_data)
+            token = user.get_token(session)
+            response.headers['Access-Token'] = token['access_token']
+            response.headers['Refresh-Token'] = token['refresh_token']
+            return {"value" : "Change Success"}
+        else:
+            return {"message": "No changes made"}
     else:
-        return {"message": "No changes made"}
-    
+        raise NotFoundException("비밀번호가 일치하지 않습니다.")
 
 def status_delete(user, session:Session):
     if not user:
